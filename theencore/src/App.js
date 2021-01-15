afd2d5e49928a7373a946beab61bb072d43ef37e
Brandon Cermak <cermakprofessional@gmail.com>
2021-01-15 11:23:31 -0600

    adding header box

--- theencore/src/App.js
+++ theencore/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchAppBar from "./components/Navbar";
 import StickyFooter from "./components/Footer";
-import PostForm from "./components/Forms"
+import PostForm from "./components/Forms";
+import Header from "./components/Header"
 
 function App() {
   return ( <div>
       <SearchAppBar />
+      <Header />
       <PostForm />
       <StickyFooter />
   </div>
---
+++ theencore/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './style.css'
+
+
+function Header() {
+  return( 
+    <div className="container">
+      <div className="row">
+        <div className="headerBox col-md-12">
+          <h1>The Encore</h1>
+          <p>Where the show continues</p>
+        </div>
+      </div>
+    </div>
+  )}
+
+export default Header;
