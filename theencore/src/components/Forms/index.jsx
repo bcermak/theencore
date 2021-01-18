82dc24a9051a52bffdfaf0f26882755a75dc3066
Brandon Cermak <cermakprofessional@gmail.com>
2021-01-18 12:12:39 -0600

    adding drop down for bands/integrating JS

--- theencore/src/components/Forms/index.jsx
+++ theencore/src/components/Forms/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import "./script"
 
 
 
@@ -8,18 +9,26 @@ export default function PostForm() {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="mainForm">
+                            <div className="btn-group float-left" id="myDropdown">
+                            <a class="btn dropdown-toggle" dataToggle="dropdown" href="#">
+                                Menu
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Choice1</a></li>
+                                <li><a href="#">Choice2</a></li>
+                                <li><a href="#">Choice3</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Choice..</a></li>
+                            </ul>
+                            </div>
                             <div className="form-group">
                                 <label for="title"> Title </label>
                                 <input required type="text" name="title" id="title" class="form-control"></input>
                             </div>
                             <div className="form-group">
-                                <label for="description"> Description </label>
+                                <label for="description"> How was the show? </label>
                                 <textarea required type="text" name="description" id="description" class="form-control">     
-                                </textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="markdown"> Markdown </label>
-                                <textarea required type="text" name="markdown" id="markdown" class="form-control">     
                                 </textarea>
                             </div>
                             <a href="/" class="btn btn-secondary">Cancel</a>
