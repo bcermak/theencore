a3fe926e56795671453efbe7aa336240a634d8e5
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-04 16:06:39 -0600

    set up routes/models folders/files

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const BlogPost = require('../models/blogPost')
+
+// Routes
+router.get('/', (req, res) => {
+ 
+    BlogPost.find({})
+        .then((data)=> {
+            console.log(data);
+            res.json(data);
+        })
+        .catch((error)=> {
+            console.log(error)
+        });
+})
+
+router.get('/api/name', (req, res) => {
+    const data = {
+        username: 'SHTSTX',
+        age: 57
+    }
+    res.json (data);
+})
+
+module.exports = router;
