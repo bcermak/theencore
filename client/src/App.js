1f94dddd2be5013bf0bc0be28edfc5326fc751b3
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-11 12:23:27 -0600

    got posts to display. fricken syntax errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,134 @@
-import React from "react";
+import React, { Component} from "react";
 import SearchAppBar from "./components/Navbar";
 import StickyFooter from "./components/Footer";
-import PostForm from "./components/Forms";
 import Header from "./components/Header"
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return ( <div>
-      <SearchAppBar />
-      <Header />
-      <PostForm />
-      <StickyFooter />
-  </div>
 
-  )}
+
+class App extends Component {
+
+  state = {
+    title:'',
+    body: '',
+    posts: []
+};
+
+
+componentDidMount = () => {
+this.getBlogPost();
+}
+
+getBlogPost = () => {
+axios.get('/api')
+  .then((response) => {
+    const data = response.data;
+    this.setState({ posts: data });
+    console.log('Data has been received');
+  })
+  .catch(() => {
+    alert('Error retrieving data!!!');
+  });
+}
+
+handleChange = ({ target }) => {
+const { name, value } = target;
+this.setState({ [name]: value });
+};
+
+submit = (event) => {
+event.preventDefault();
+
+const payload = {
+    title: this.state.title,
+    body: this.state.body
+};
+
+axios({
+    url: '/api/save',
+    method: 'POST',
+    data: payload
+})
+.then(() => {
+    console.log('Data has been sent to the server');
+    this.resetUserInputs();
+})
+.catch(() => {
+    console.log('Internal Server Error!');
+})
+}
+
+resetUserInputs = () => {
+this.setState({
+    title: '',
+    body: ''
+})
+};
+
+displayPost = (posts) => {
+  if (!posts.length) return null;
+
+  return posts.map((post, index) => (
+    <div key={index}>
+        <h3> {post.title}</h3>
+        <p> {post.body}</p>
+    </div>
+
+
+));
+
+};
+
+  render(){   
+    console.log('State:', this.state)
+    return (
+      <div>
+        <SearchAppBar />
+        <Header />
+        <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <form 
+                            className="mainForm"
+                            onSubmit={this.submit}>
+                            <div className="form-input">
+                                <input
+                                required 
+                                type="text" 
+                                name="title"
+                                placeholder="Enter Title" 
+                                value={this.state.title}
+                                onChange={this.handleChange}   
+                                />
+                            </div>
+                            <div className="form-input">
+                                <textarea 
+                                required name="body" 
+                                placeholder="How was the show?" 
+                                cols="30" row="10" 
+                                value={this.state.body}
+                                onChange={this.handleChange}  
+                                ></textarea>
+                            </div>
+                            <a href="/" className="btn btn-secondary">Cancel</a>
+                            <button type="submit" className="btn btn-primary">Save</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="blog-">
+                            {this.displayPost(this.state.posts)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <StickyFooter />
+      </div>
+
+    );
+  };
+};
 
 export default App;
